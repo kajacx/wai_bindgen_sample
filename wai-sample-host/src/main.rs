8864f8e61e87bf103278d0937d77b5eb7c4710f6
kajacx <kajacx@gmail.com>
2023-03-28 21:59:18 +0200

    wasmtime works

--- wai-sample-host/src/main.rs
+++ wai-sample-host/src/main.rs
@@ -1,5 +1,7 @@
-use sample_protocol_plugin::{SampleProtocolPlugin, SampleProtocolPluginData};
-use wai_bindgen_wasmtime::wasmtime::{Config, Engine, Linker, Module, Store};
+use sample_protocol_plugin::SampleProtocolPluginData;
+use wai_bindgen_wasmtime::wasmtime::{
+    Config, Engine, Func, FuncType, Linker, Module, Store, Val, ValType,
+};
 
 const PLUGIN_BYTES: &'static [u8] = include_bytes!(
     "../../wai-sample-plugin/target/wasm32-unknown-unknown/debug/wai_sample_plugin.wasm"
@@ -19,33 +21,26 @@ impl sample_protocol_host::SampleProtocolHost for SampleProtocolHost {
 type MyT = SampleProtocolPluginData;
 
 fn main() {
-    // let compiler = ::wasmer::Cranelift::default();
-    // let engine = ::wasmer::Universal::new(compiler).engine();
-    // let mut store = Store::new(unsafe { std::mem::transmute(&engine) }); //wasmer and wai_bindgen_wasmer are in conflict?
-
-    // let module = Module::new(&store, PLUGIN_BYTES).expect("should create module");
-
-    // let mut imports = imports! {};
-
-    // let (sample, _instance) = SampleProtocolPlugin::instantiate(&mut store, &module, &mut imports)
-    //     .expect("should create instance");
-
-    // println!("{:?}", sample.add_three(&mut store, 5));
-
     let config = Config::new();
     let engine = Engine::new(&config).expect("should create engine");
 
     let mut store = Store::new(&engine, SampleProtocolPluginData::default());
 
     let mut linker = Linker::<MyT>::new(&engine);
-    // let wasi_state = WasiS::new("wasi_snapshot_preview1")
-    //     .finalize()
-    //     .expect("failed to initialize WASI");
-    // wasi_state
-    //     .add_to_linker(&mut linker)
-    //     .expect("failed to link WASI");
-
-    //let mut data = SampleProtocolPluginData::default();
+    linker
+        .define(
+            "sample-protocol-host",
+            "add-one",
+            Func::new(
+                &mut store,
+                FuncType::new([ValType::I32], [ValType::I32]),
+                |_, args, ret| {
+                    ret[0] = Val::I32(args[0].unwrap_i32() + 1);
+                    Ok(())
+                },
+            ),
+        )
+        .expect("should define a function");
 
     let module = Module::new(&engine, PLUGIN_BYTES).expect("should load module from bytes");
 
