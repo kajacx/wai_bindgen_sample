b5308c303a125514e15e204b50e991d5d8578467
kajacx <kajacx@gmail.com>
2023-03-28 21:52:27 +0200

    wasmtime seems better

--- wai-sample-host/src/main.rs
+++ wai-sample-host/src/main.rs
@@ -1,11 +1,11 @@
-use sample_protocol_plugin::SampleProtocolPlugin;
-use wai_bindgen_wasmer::wasmer::*;
+use sample_protocol_plugin::{SampleProtocolPlugin, SampleProtocolPluginData};
+use wai_bindgen_wasmtime::wasmtime::{Config, Engine, Linker, Module, Store};
 
 const PLUGIN_BYTES: &'static [u8] = include_bytes!(
     "../../wai-sample-plugin/target/wasm32-unknown-unknown/debug/wai_sample_plugin.wasm"
 );
 
-wai_bindgen_wasmer::import!("../sample-protocol-plugin.wai");
+wai_bindgen_wasmtime::import!("../sample-protocol-plugin.wai");
 wai_bindgen_rust::export!("../sample-protocol-host.wai");
 
 struct SampleProtocolHost;
@@ -16,17 +16,46 @@ impl sample_protocol_host::SampleProtocolHost for SampleProtocolHost {
     }
 }
 
+type MyT = SampleProtocolPluginData;
+
 fn main() {
-    let compiler = ::wasmer::Cranelift::default();
-    let engine = ::wasmer::Universal::new(compiler).engine();
-    let mut store = Store::new(unsafe { std::mem::transmute(&engine) }); //wasmer and wai_bindgen_wasmer are in conflict?
+    // let compiler = ::wasmer::Cranelift::default();
+    // let engine = ::wasmer::Universal::new(compiler).engine();
+    // let mut store = Store::new(unsafe { std::mem::transmute(&engine) }); //wasmer and wai_bindgen_wasmer are in conflict?
 
-    let module = Module::new(&store, PLUGIN_BYTES).expect("should create module");
+    // let module = Module::new(&store, PLUGIN_BYTES).expect("should create module");
 
-    let mut imports = imports! {};
+    // let mut imports = imports! {};
 
-    let (sample, _instance) = SampleProtocolPlugin::instantiate(&mut store, &module, &mut imports)
-        .expect("should create instance");
+    // let (sample, _instance) = SampleProtocolPlugin::instantiate(&mut store, &module, &mut imports)
+    //     .expect("should create instance");
 
-    println!("{:?}", sample.add_three(&mut store, 5));
+    // println!("{:?}", sample.add_three(&mut store, 5));
+
+    let config = Config::new();
+    let engine = Engine::new(&config).expect("should create engine");
+
+    let mut store = Store::new(&engine, SampleProtocolPluginData::default());
+
+    let mut linker = Linker::<MyT>::new(&engine);
+    // let wasi_state = WasiS::new("wasi_snapshot_preview1")
+    //     .finalize()
+    //     .expect("failed to initialize WASI");
+    // wasi_state
+    //     .add_to_linker(&mut linker)
+    //     .expect("failed to link WASI");
+
+    //let mut data = SampleProtocolPluginData::default();
+
+    let module = Module::new(&engine, PLUGIN_BYTES).expect("should load module from bytes");
+
+    let (plugin, _instance) = sample_protocol_plugin::SampleProtocolPlugin::instantiate(
+        &mut store,
+        &module,
+        &mut linker,
+        |data| data,
+    )
+    .expect("should create instance");
+
+    println!("{:?}", plugin.add_three(&mut store, 5));
 }
