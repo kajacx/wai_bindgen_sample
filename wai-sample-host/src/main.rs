bdaf04b281a6188a88bc3492543cb8a2a328d78d
kajacx <kajacx@gmail.com>
2023-03-28 22:14:29 +0200

    Use sample_protocol_host::add_to_linker

--- wai-sample-host/src/main.rs
+++ wai-sample-host/src/main.rs
@@ -1,24 +1,18 @@
 use sample_protocol_plugin::SampleProtocolPluginData;
-use wai_bindgen_wasmtime::wasmtime::{
-    Config, Engine, Func, FuncType, Linker, Module, Store, Val, ValType,
-};
+use wai_bindgen_wasmtime::wasmtime::{Config, Engine, Linker, Module, Store};
 
 const PLUGIN_BYTES: &'static [u8] = include_bytes!(
     "../../wai-sample-plugin/target/wasm32-unknown-unknown/debug/wai_sample_plugin.wasm"
 );
 
 wai_bindgen_wasmtime::import!("../sample-protocol-plugin.wai");
-wai_bindgen_rust::export!("../sample-protocol-host.wai");
+wai_bindgen_wasmtime::export!("../sample-protocol-host.wai");
 
-struct SampleProtocolHost;
-
-impl sample_protocol_host::SampleProtocolHost for SampleProtocolHost {
-    fn add_one(num: u32) -> u32 {
+impl sample_protocol_host::SampleProtocolHost for SampleProtocolPluginData {
+    fn add_one(&mut self, num: u32) -> u32 {
         num + 1
     }
 }
-
-type MyT = SampleProtocolPluginData;
 
 fn main() {
     let config = Config::new();
@@ -26,21 +20,8 @@ fn main() {
 
     let mut store = Store::new(&engine, SampleProtocolPluginData::default());
 
-    let mut linker = Linker::<MyT>::new(&engine);
-    linker
-        .define(
-            "sample-protocol-host",
-            "add-one",
-            Func::new(
-                &mut store,
-                FuncType::new([ValType::I32], [ValType::I32]),
-                |_, args, ret| {
-                    ret[0] = Val::I32(args[0].unwrap_i32() + 1);
-                    Ok(())
-                },
-            ),
-        )
-        .expect("should define a function");
+    let mut linker = Linker::<SampleProtocolPluginData>::new(&engine);
+    sample_protocol_host::add_to_linker(&mut linker, |data| data).expect("should link host fns");
 
     let module = Module::new(&engine, PLUGIN_BYTES).expect("should load module from bytes");
 
