838798892093293694c0aba408e9652abde1449f
kajacx <kajacx@gmail.com>
2023-03-28 20:44:29 +0200

    Try to make wasmer store

--- wai-sample-host/src/main.rs
+++ wai-sample-host/src/main.rs
@@ -1,12 +1,12 @@
-wai_bindgen_wasmer::import!("../sample-protocol-plugin.wai");
-wai_bindgen_rust::export!("../sample-protocol-host.wai");
-
 use sample_protocol_plugin::SampleProtocolPlugin;
 use wai_bindgen_wasmer::wasmer::*;
 
 const PLUGIN_BYTES: &'static [u8] = include_bytes!(
     "../../wai-sample-plugin/target/wasm32-unknown-unknown/debug/wai_sample_plugin.wasm"
 );
+
+wai_bindgen_wasmer::import!("../sample-protocol-plugin.wai");
+wai_bindgen_rust::export!("../sample-protocol-host.wai");
 
 struct SampleProtocolHost;
 
@@ -17,7 +17,9 @@ impl sample_protocol_host::SampleProtocolHost for SampleProtocolHost {
 }
 
 fn main() {
-    let mut store = Store::new(Comp);
+    let compiler = ::wasmer::Cranelift::default();
+    let engine = ::wasmer::Universal::new(compiler).engine();
+    let mut store = Store::new(&engine); //wasmer and wai_bindgen_wasmer are in conflict?
 
     let module = Module::new(&store, PLUGIN_BYTES).expect("should create module");
 
