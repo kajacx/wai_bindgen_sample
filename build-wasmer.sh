ceda31a4e4c1275b23ab033f9f47839e7d2547b0
kajacx <kajacx@gmail.com>
2023-03-29 11:06:25 +0200

    Split wasmtime and wasmer hosts

--- build-wasmer.sh
+++ build-wasmer.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 
 cd wai-sample-plugin && cargo build --target=wasm32-unknown-unknown && cd .. && \
-cd wai-sample-host && cargo run && cd .. && \
+cd wai-sample-wasmer-host && cargo run && cd .. && \
 echo All done
